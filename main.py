ed2c7e6fa11a8754152f2660b4f51d3ded204ee8
22125 <22125@students.mrgs.school.nz>
2026-04-23 09:54:27 +1200

    Imported PIL and tkinter and started set up of loginpage

---
+++ main.py
@@ -0,0 +1,15 @@
+from tkinter import*
+from PIL import Image, ImageTk # imports modules for image resizing and display
+
+
+class LoginPage:
+ def __init__(self, parent):
+     background_color = "#1D61BB"  # sets the background colour
+     self.login_frame = Frame(parent, bg=background_color, padx=100, pady=100)
+     self.login_frame.grid()
+     
+
+
+
+
+
