1bac092de0413fae91400eda7abff179602400e3
22125 <22125@students.mrgs.school.nz>
2026-05-15 14:58:28 +1200

    removed entry box section

--- main.py
+++ main.py
@@ -15,9 +15,7 @@ class LoginPage:
         self.image_label = Label(self.login_frame, image=self.photo, bg=background_color)
         self.image_label.grid(row=0, column=0, pady=(1, 50))
         # entry box for the user to input their username
-        self.entry_box = Entry(self.login_frame, width=20, font=("Lilita one", 16), )
-        self.entry_box.grid(row=3, column=0, pady=20, )
-        self.entry_box.bind("<KeyRelease>", self.validate_name)
+        self.
         # label to display the error message for if the user types out an invalid name
         self.error_label = Label(self.login_frame, text="", fg="red", bg=background_color, font=("Verdana", 11))
         self.error_label.grid(row=4, column=0)
